28dd06ec3e175288ad284b6994097b003ad5e068
Muriel <walkerabyssaa@gmail.com>
2023-04-22 12:25:37 +0700

    pack.py done

--- pack.py
+++ pack.py
@@ -1,4 +1,4 @@
-# Создаем грфаический элемент
+# Кнопки
 from tkinter import *
 
 
@@ -34,61 +34,62 @@ class Paint(Frame):
 
     def setUI(self):
 
-        self.parent.title("PAINt")  # Устанавливаем название окна
+        self.parent.title("Pythonicway PyPaint")  # Устанавливаем название окна
         self.pack(fill=BOTH, expand=1)  # Размещаем активные элементы на родительском окне
 
-        self.columnconfigure(10, weight=1) # Даем седьмому столбцу возможность растягиваться, благодаря чему кнопки не будут разъезжаться при ресайзе
+        self.columnconfigure(9, weight=1) # Даем седьмому столбцу возможность растягиваться, благодаря чему кнопки не будут разъезжаться при ресайзе
         self.rowconfigure(2, weight=1) # То же самое для третьего ряда
 
         self.canv = Canvas(self, bg="white")  # Создаем поле для рисования, устанавливаем белый фон
-        self.canv.grid(row=2, column=0, columnspan=11, padx=5, pady=5, sticky=E+W+S+N)  # Прикрепляем канвас методом grid. Он будет находится в 3м ряду, первой колонке, и будет занимать 7 колонок, задаем отступы по X и Y в 5 пикселей, и заставляем растягиваться при растягивании всего окна
+        self.canv.pack(expand=1, fill=BOTH)  # Прикрепляем канвас методом pack. Он будет находится в 3м ряду, первой колонке, и будет занимать 7 колонок, задаем отступы по X и Y в 5 пикселей, и заставляем растягиваться при растягивании всего окна
         self.canv.bind("<B1-Motion>", self.draw) # Привязываем обработчик к канвасу. <B1-Motion> означает "при движении зажатой левой кнопки мыши" вызывать функцию draw
 
         color_lab = Label(self, text="Color: ") # Создаем метку для кнопок изменения цвета кисти
-        color_lab.grid(row=0, column=0, padx=6) # Устанавливаем созданную метку в первый ряд и первую колонку, задаем горизонтальный отступ в 6 пикселей
+        color_lab.pack(side=LEFT) # Устанавливаем созданную метку в первый ряд и первую колонку, задаем горизонтальный отступ в 6 пикселей
 
-        red_btn = Button(self, width=5, bg='red', command=lambda: self.set_color('red')) # Создание кнопки:  Установка текста кнопки, задание ширины кнопки (10 символов), функция вызываемая при нажатии кнопки.
-        red_btn.grid(row=0, column=1) # Устанавливаем кнопку
+        red_btn = Button(self, width=5, bg='red',command=lambda: self.set_color('red')) # Создание кнопки:  Установка текста кнопки, задание ширины кнопки (10 символов), функция вызываемая при нажатии кнопки.
+        red_btn.pack(side=LEFT) # Устанавливаем кнопку
 
         # Кнопочки
 
         green_btn = Button(self, width=5, bg='green', command=lambda: self.set_color("green"))
-        green_btn.grid(row=0, column=2)
+        green_btn.pack(side=LEFT)
 
         blue_btn = Button(self, width=5, bg='blue', command=lambda: self.set_color("blue"))
-        blue_btn.grid(row=0, column=3)
+        blue_btn.pack(side=LEFT)
 
         black_btn = Button(self, width=5, bg='black', command=lambda: self.set_color("black"))
-        black_btn.grid(row=1, column=2)
+        black_btn.pack(side=LEFT)
 
         white_btn = Button(self, width=5, bg='white', command=lambda: self.set_color("white"))
-        white_btn.grid(row=1, column=1)
+        white_btn.pack(side=LEFT)
 
         yellow_btn = Button(self, width=5, bg='yellow', command=lambda: self.set_color("yellow"))
-        yellow_btn.grid(row=1, column=3)
+        yellow_btn.pack(side=LEFT)
 
         clear_btn = Button(self, text="Clear all", width=10, command=lambda: self.canv.delete("all"))
-        clear_btn.grid(row=1, column=9)
-
+        clear_btn.pack(side=LEFT)
+############################################
         size_lab = Label(self, text="Brush size: ")
-        size_lab.grid(row=0, column=5, padx=5)
+        size_lab.pack(side=LEFT)
+
         one_btn = Button(self, text="2", width=5, command=lambda: self.set_brush_size(2))
-        one_btn.grid(row=0, column=6)
+        one_btn.pack(side=LEFT)
 
         two_btn = Button(self, text="5", width=5, command=lambda: self.set_brush_size(5))
-        two_btn.grid(row=0, column=7)
+        two_btn.pack(side=LEFT)
 
         five_btn = Button(self, text="7", width=5, command=lambda: self.set_brush_size(7))
-        five_btn.grid(row=0, column=8)
+        five_btn.pack(side=LEFT)
 
         seven_btn = Button(self, text="10", width=5, command=lambda: self.set_brush_size(10))
-        seven_btn.grid(row=1, column=6)
+        seven_btn.pack(side=LEFT)
 
         ten_btn = Button(self, text="12", width=5, command=lambda: self.set_brush_size(20))
-        ten_btn.grid(row=1, column=7)
+        ten_btn.pack(side=LEFT)
 
         twenty_btn = Button(self, text="15", width=5, command=lambda: self.set_brush_size(50))
-        twenty_btn.grid(row=1, column=8)
+        twenty_btn.pack(side=LEFT)
 
         btns = [red_btn, green_btn]
 
